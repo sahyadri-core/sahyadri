a240afe0cf9ec4dccba42ae70d92f50689734d1a
Suraj Datir <206947790+surajdatir@users.noreply.github.com>
2026-07-01 20:40:38 +0530

    Update consensus/wasm: utils.rs

--- consensus/wasm/src/utils.rs
+++ consensus/wasm/src/utils.rs
@@ -1,5 +1,5 @@
 use crate::result::Result;
-use sahyadri_consensus_core::hashing::sighash::{SigHashReusedValuesUnsync, calc_schnorr_signature_hash};
+use sahyadri_consensus_core::hashing::sighash::{SigHashReusedValuesUnsync, calc_signature_hash};
 use sahyadri_consensus_core::hashing::sighash_type::SIG_HASH_ALL;
 use sahyadri_consensus_core::tx;
 
@@ -11,7 +11,7 @@ pub fn script_hashes(mut mutable_tx: tx::SignableTransaction) -> Result<Vec<sahy
 
     let reused_values = SigHashReusedValuesUnsync::new();
     for i in 0..mutable_tx.tx.inputs.len() {
-        let sig_hash = calc_schnorr_signature_hash(&mutable_tx.as_verifiable(), i, SIG_HASH_ALL, &reused_values);
+        let sig_hash = calc_signature_hash(&mutable_tx.as_verifiable(), i, SIG_HASH_ALL, &reused_values);
         list.push(sig_hash);
     }
     Ok(list)
