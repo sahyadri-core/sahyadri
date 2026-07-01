538dbee62827f6dd506821c62570860b88eb4846
Suraj Datir <206947790+surajdatir@users.noreply.github.com>
2026-07-01 20:40:39 +0530

    Update wallet/keys: pubkeygen.rs

--- wallet/keys/src/pubkeygen.rs
+++ wallet/keys/src/pubkeygen.rs
@@ -55,13 +55,13 @@ impl PublicKeyGenerator {
             (start, end) = (end, start);
         }
         let pubkeys = self.hd_wallet.receive_pubkey_manager().derive_pubkey_range(start..end)?;
-        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(pk)))).unchecked_into())
+        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(&pk.0[..])))).unchecked_into())
     }
 
     /// Generate a single Receive Public Key derivation at a given index.
     #[wasm_bindgen(js_name=receivePubkey)]
     pub fn receive_pubkey(&self, index: u32) -> Result<PublicKey> {
-        Ok(self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?.into())
+        Ok(PublicKey::from(&self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?.0[..]))
     }
 
     /// Generate a range of Receive Public Key derivations and return them as strings.
@@ -71,13 +71,13 @@ impl PublicKeyGenerator {
             (start, end) = (end, start);
         }
         let pubkeys = self.hd_wallet.receive_pubkey_manager().derive_pubkey_range(start..end)?;
-        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(pk).to_string()))).unchecked_into())
+        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(&pk.0[..]).to_string()))).unchecked_into())
     }
 
     /// Generate a single Receive Public Key derivation at a given index and return it as a string.
     #[wasm_bindgen(js_name=receivePubkeyAsString)]
     pub fn receive_pubkey_as_string(&self, index: u32) -> Result<String> {
-        Ok(self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?.to_string())
+         Ok(faster_hex::hex_string(&self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?.0))
     }
 
     /// Generate Receive Address derivations for a given range.
@@ -90,7 +90,7 @@ impl PublicKeyGenerator {
         let network_type = NetworkType::try_from(networkType)?;
         let pubkeys = self.hd_wallet.receive_pubkey_manager().derive_pubkey_range(start..end)?;
         let addresses =
-            pubkeys.into_iter().map(|pk| PublicKey::from(pk).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
+            pubkeys.into_iter().map(|pk| PublicKey::from(&pk.0[..]).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
         Ok(Array::from_iter(addresses.into_iter().map(JsValue::from)).unchecked_into())
     }
 
@@ -98,7 +98,7 @@ impl PublicKeyGenerator {
     #[wasm_bindgen(js_name=receiveAddress)]
     #[allow(non_snake_case)]
     pub fn receive_address(&self, networkType: &NetworkTypeT, index: u32) -> Result<Address> {
-        PublicKey::from(self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?).to_address(networkType.try_into()?)
+        PublicKey::from(&self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?.0[..]).to_address(networkType.try_into()?)
     }
 
     /// Generate a range of Receive Address derivations and return them as strings.
@@ -111,7 +111,7 @@ impl PublicKeyGenerator {
         let network_type = NetworkType::try_from(networkType)?;
         let pubkeys = self.hd_wallet.receive_pubkey_manager().derive_pubkey_range(start..end)?;
         let addresses =
-            pubkeys.into_iter().map(|pk| PublicKey::from(pk).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
+            pubkeys.into_iter().map(|pk| PublicKey::from(&pk.0[..]).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
         Ok(Array::from_iter(addresses.into_iter().map(String::from).map(JsValue::from)).unchecked_into())
     }
 
@@ -119,7 +119,7 @@ impl PublicKeyGenerator {
     #[wasm_bindgen(js_name=receiveAddressAsString)]
     #[allow(non_snake_case)]
     pub fn receive_address_as_string(&self, networkType: &NetworkTypeT, index: u32) -> Result<String> {
-        Ok(PublicKey::from(self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?)
+        Ok(PublicKey::from(&self.hd_wallet.receive_pubkey_manager().derive_pubkey(index)?.0[..])
             .to_address(networkType.try_into()?)?
             .to_string())
     }
@@ -133,13 +133,13 @@ impl PublicKeyGenerator {
             (start, end) = (end, start);
         }
         let pubkeys = self.hd_wallet.change_pubkey_manager().derive_pubkey_range(start..end)?;
-        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(pk)))).unchecked_into())
+        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(&pk.0[..])))).unchecked_into())
     }
 
     /// Generate a single Change Public Key derivation at a given index.
     #[wasm_bindgen(js_name=changePubkey)]
     pub fn change_pubkey(&self, index: u32) -> Result<PublicKey> {
-        Ok(self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.into())
+        Ok(PublicKey::from(&self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.0[..]))
     }
 
     /// Generate a range of Change Public Key derivations and return them as strings.
@@ -149,13 +149,13 @@ impl PublicKeyGenerator {
             (start, end) = (end, start);
         }
         let pubkeys = self.hd_wallet.change_pubkey_manager().derive_pubkey_range(start..end)?;
-        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(pk).to_string()))).unchecked_into())
+        Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(&pk.0[..]).to_string()))).unchecked_into())
     }
 
     /// Generate a single Change Public Key derivation at a given index and return it as a string.
     #[wasm_bindgen(js_name=changePubkeyAsString)]
     pub fn change_pubkey_as_string(&self, index: u32) -> Result<String> {
-        Ok(self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.to_string())
+        Ok(faster_hex::hex_string(&self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.0))
     }
 
     /// Generate Change Address derivations for a given range.
@@ -168,7 +168,7 @@ impl PublicKeyGenerator {
         let network_type = NetworkType::try_from(networkType)?;
         let pubkeys = self.hd_wallet.change_pubkey_manager().derive_pubkey_range(start..end)?;
         let addresses =
-            pubkeys.into_iter().map(|pk| PublicKey::from(pk).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
+            pubkeys.into_iter().map(|pk| PublicKey::from(&pk.0[..]).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
         Ok(Array::from_iter(addresses.into_iter().map(JsValue::from)).unchecked_into())
     }
 
@@ -176,7 +176,7 @@ impl PublicKeyGenerator {
     #[wasm_bindgen(js_name=changeAddress)]
     #[allow(non_snake_case)]
     pub fn change_address(&self, networkType: &NetworkTypeT, index: u32) -> Result<Address> {
-        PublicKey::from(self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?).to_address(networkType.try_into()?)
+        PublicKey::from(&self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.0[..]).to_address(networkType.try_into()?)
     }
 
     /// Generate a range of Change Address derivations and return them as strings.
@@ -189,7 +189,7 @@ impl PublicKeyGenerator {
         let network_type = NetworkType::try_from(networkType)?;
         let pubkeys = self.hd_wallet.change_pubkey_manager().derive_pubkey_range(start..end)?;
         let addresses =
-            pubkeys.into_iter().map(|pk| PublicKey::from(pk).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
+            pubkeys.into_iter().map(|pk| PublicKey::from(&pk.0[..]).to_address(network_type)).collect::<Result<Vec<Address>>>()?;
         Ok(Array::from_iter(addresses.into_iter().map(String::from).map(JsValue::from)).unchecked_into())
     }
 
@@ -197,7 +197,7 @@ impl PublicKeyGenerator {
     #[wasm_bindgen(js_name=changeAddressAsString)]
     #[allow(non_snake_case)]
     pub fn change_address_as_string(&self, networkType: &NetworkTypeT, index: u32) -> Result<String> {
-        Ok(PublicKey::from(self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?)
+        Ok(PublicKey::from(&self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.0[..])
             .to_address(networkType.try_into()?)?
             .to_string())
     }
@@ -208,12 +208,12 @@ impl PublicKeyGenerator {
     //         (start, end) = (end, start);
     //     }
     //     let pubkeys = self.hd_wallet.change_pubkey_manager().derive_pubkey_range(start..end)?;
-    //     Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(pk)))).unchecked_into())
+    //     Ok(Array::from_iter(pubkeys.into_iter().map(|pk| JsValue::from(PublicKey::from(&pk.0[..])))).unchecked_into())
     // }
 
     // #[wasm_bindgen(js_name=changePubkey)]
     // pub fn change_pubkey(&self, index: u32) -> Result<PublicKey> {
-    //     Ok(self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.into())
+    //     Ok(PublicKey::from(&self.hd_wallet.change_pubkey_manager().derive_pubkey(index)?.0[..]))
     // }
 
     #[wasm_bindgen(js_name=toString)]
