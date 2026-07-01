efad7cfb6301ae41a309d0fab3d2f401ee5d0603
Suraj Datir <206947790+surajdatir@users.noreply.github.com>
2026-07-01 20:40:39 +0530

    Update wallet/keys: keypair.rs

--- wallet/keys/src/keypair.rs
+++ wallet/keys/src/keypair.rs
@@ -1,104 +1,58 @@
 //!
 //! [`keypair`](mod@self) module encapsulates [`Keypair`] and [`PrivateKey`].
-//! The [`Keypair`] provides access to the secret and public keys.
-//!
-//! # JavaScript Example
-//!
-//! ```javascript
-//!
-//! let keypair = Keypair.random();
-//! let privateKey = keypair.privateKey;
-//! let publicKey = keypair.publicKey;
-//!
-//! // to obtain an address from a keypair
-//! let address = keypair.toAddress(NetworkType.Mainnnet);
-//!
-//! // to obtain a keypair from a private key
-//! let keypair = privateKey.toKeypair();
-//!
-//! ```
+//! The [`Keypair`] provides access to the secret and public keys (Dilithium3).
 //!
 
 use crate::imports::*;
-use secp256k1::{Secp256k1, XOnlyPublicKey};
-use serde_wasm_bindgen::to_value;
+use sahyadri_dilithium::{DilithiumKeyPair, generate_keypair, generate_keypair_from_seed};
+use sha2::{Sha256, Digest};
 
-/// Data structure that contains a secret and public keys.
+/// Data structure that contains a Dilithium keypair.
 /// @category Wallet SDK
-#[derive(Debug, Clone, CastFromJs)]
+#[derive(Clone, CastFromJs)]
 #[wasm_bindgen(inspectable)]
 pub struct Keypair {
-    secret_key: secp256k1::SecretKey,
-    public_key: secp256k1::PublicKey,
-    xonly_public_key: XOnlyPublicKey,
+    inner: DilithiumKeyPair,
 }
 
 #[wasm_bindgen]
 impl Keypair {
-    fn new(secret_key: secp256k1::SecretKey, public_key: secp256k1::PublicKey, xonly_public_key: XOnlyPublicKey) -> Self {
-        Self { secret_key, public_key, xonly_public_key }
+    fn new(inner: DilithiumKeyPair) -> Self {
+        Self { inner }
     }
 
     /// Get the [`PublicKey`] of this [`Keypair`].
     #[wasm_bindgen(getter = publicKey)]
     pub fn get_public_key(&self) -> String {
-        PublicKey::from(&self.public_key).to_string()
+        self.inner.public_key().to_vec().to_hex()
     }
 
-    /// Get the [`PrivateKey`] of this [`Keypair`].
+    /// Get the [`PrivateKey`] seed of this [`Keypair`].
     #[wasm_bindgen(getter = privateKey)]
     pub fn get_private_key(&self) -> String {
-        PrivateKey::from(&self.secret_key).to_hex()
+        self.inner.private_key()[..32].to_vec().to_hex()
     }
 
-    /// Get the `XOnlyPublicKey` of this [`Keypair`].
-    #[wasm_bindgen(getter = xOnlyPublicKey)]
-    pub fn get_xonly_public_key(&self) -> JsValue {
-        to_value(&self.xonly_public_key).unwrap()
-    }
-
-    /// Get the [`Address`] of this Keypair's [`PublicKey`].
-    /// Receives a [`NetworkType`](sahyadri_consensus_core::network::NetworkType)
-    /// to determine the prefix of the address.
-    /// JavaScript: `let address = keypair.toAddress(NetworkType.MAINNET);`.
+    /// Get the [`Address`] of this Keypair's Dilithium public key.
     #[wasm_bindgen(js_name = toAddress)]
-    // pub fn to_address(&self, network_type: NetworkType) -> Result<Address> {
     pub fn to_address(&self, network: &NetworkTypeT) -> Result<Address> {
-        let payload = &self.xonly_public_key.serialize();
-        let address = Address::new(network.try_into()?, AddressVersion::PubKey, payload);
-        Ok(address)
-    }
-
-    /// Get `ECDSA` [`Address`] of this Keypair's [`PublicKey`].
-    /// Receives a [`NetworkType`](sahyadri_consensus_core::network::NetworkType)
-    /// to determine the prefix of the address.
-    /// JavaScript: `let address = keypair.toAddress(NetworkType.MAINNET);`.
-    #[wasm_bindgen(js_name = toAddressECDSA)]
-    pub fn to_address_ecdsa(&self, network: &NetworkTypeT) -> Result<Address> {
-        let payload = &self.public_key.serialize();
-        let address = Address::new(network.try_into()?, AddressVersion::PubKeyECDSA, payload);
+        let payload = &Sha256::digest(self.inner.public_key())[..20];
+        let address = Address::new(network.try_into()?, AddressVersion::PubKeyDilithium, payload);
         Ok(address)
     }
 
     /// Create a new random [`Keypair`].
-    /// JavaScript: `let keypair = Keypair::random();`.
     #[wasm_bindgen]
     pub fn random() -> Result<Keypair, JsError> {
-        let secp = Secp256k1::new();
-        let (secret_key, public_key) = secp.generate_keypair(&mut rand::thread_rng());
-        let (xonly_public_key, _) = public_key.x_only_public_key();
-        Ok(Keypair::new(secret_key, public_key, xonly_public_key))
+        let kp = generate_keypair().map_err(|e| JsError::new(&e.to_string()))?;
+        Ok(Keypair::new(kp))
     }
 
-    /// Create a new [`Keypair`] from a [`PrivateKey`].
-    /// JavaScript: `let privkey = new PrivateKey(hexString); let keypair = privkey.toKeypair();`.
+    /// Create a new [`Keypair`] from a [`PrivateKey`] seed.
     #[wasm_bindgen(js_name = "fromPrivateKey")]
     pub fn from_private_key(secret_key: &PrivateKey) -> Result<Keypair, JsError> {
-        let secp = Secp256k1::new();
-        let secret_key = secp256k1::SecretKey::from_slice(&secret_key.secret_bytes())?;
-        let public_key = secp256k1::PublicKey::from_secret_key(&secp, &secret_key);
-        let (xonly_public_key, _) = public_key.x_only_public_key();
-        Ok(Keypair::new(secret_key, public_key, xonly_public_key))
+        let kp = generate_keypair_from_seed(secret_key.seed_bytes());
+        Ok(Keypair::new(kp))
     }
 }
 
