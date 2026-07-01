201407c22fbe0113fc5fd9d97caf8bde7f8d2557
Suraj Datir <206947790+surajdatir@users.noreply.github.com>
2026-07-01 20:40:38 +0530

    Update rpc/wrpc: lib.rs

--- rpc/wrpc/client/src/lib.rs
+++ rpc/wrpc/client/src/lib.rs
@@ -1,13 +1,13 @@
 //!
-//! # wRPC Client for Rusty Sahyadri p2p Node
+//! # wRPC Client for Sahyadri p2p Node
 //!
-//! This crate provides a WebSocket RPC client for Rusty Sahyadri p2p node. It is based on the
+//! This crate provides a WebSocket RPC client for Sahyadri p2p node. It is based on the
 //! [wRPC](https://docs.rs/workflow-rpc) crate that offers WebSocket RPC implementation
 //! for Rust based on Borsh and Serde JSON serialization. wRPC is a lightweight RPC framework
 //! meant to function as an IPC (Inter-Process Communication) mechanism for Rust applications.
 //!
 //! Rust examples on using wRPC client can be found in the
-//! [examples](https://github.com/sahyadrinet/rusty-sahyadri/tree/master/rpc/wrpc/examples) folder.
+//! [examples](https://github.com/sahyadrinet/sahyadri/tree/master/rpc/wrpc/examples) folder.
 //!
 //! WASM bindings for wRPC client can be found in the [`sahyadri-wrpc-wasm`](https://docs.rs/sahyadri-wrpc-wasm) crate.
 //!
